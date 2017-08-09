b807fe6a4d456877b80f0b161ae437091a0b8c5d
Zhong Weibin <weibin.zhong@maxent-inc.com>
2017-08-09 17:11:56 +0800

    optimize router and add no-match page

---
+++ src/common/noMatch/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by geekzwb on 2017/8/9.
+ * What for: NoMatch
+ */
+import React from 'react';
+
+const NoMatch = () => {
+  return (
+    <div>
+        <h1 style={{marginTop: '300px', textAlign: 'center'}}>404 No Found !</h1>
+    </div>
+  )
+};
+
+export default NoMatch;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import {ConnectedRouter} from 'react-router-redux';
 import registerServiceWorker from './registerServiceWorker';
 import {history} from './redux/store';
 import store from './redux/store';
 import Dashboard from './modules/dashboard/Dashboard';
 import App from './App';
+import NoMatch from './common/noMatch';
 
 // ================================
 // 将根组件挂载到 DOM，启动！
@@ -15,10 +16,11 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
-        <Route exact path="/" component={App} />
-        <Route path="/dashboard" component={Dashboard} />
-      </div>
+        <Switch>
+          <Route exact path="/" component={App}/>
+          <Route path="/dashboard" component={Dashboard}/>
+          <Route component={NoMatch}/>
+        </Switch>
     </ConnectedRouter>
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
